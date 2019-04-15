81a47350de8123ee2731e99032f3da1989248ffc
Jarrod <thejarroddoyle@gmail.com>
2019-04-15 23:46:17 +0100

    Distance based scoring added
    Scoring system uses basic Dijkstra shortest path implementation to choose moves that put MrX as far away from detectives as possible.

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/JJMrX.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/JJMrX.java
@@ -1,10 +1,13 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Set;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Collections;
 import java.util.function.Consumer;
 
 import uk.ac.bris.cs.scotlandyard.ai.ManagedAI;
@@ -19,6 +22,7 @@ import uk.ac.bris.cs.scotlandyard.model.ScotlandYardView;
 import uk.ac.bris.cs.scotlandyard.model.Transport;
 
 import uk.ac.bris.cs.gamekit.graph.Edge;
+import uk.ac.bris.cs.gamekit.graph.Node;
 import uk.ac.bris.cs.gamekit.graph.Graph;
 
 @ManagedAI("JJ-MrX")
@@ -60,10 +64,62 @@ public class JJMrX implements PlayerFactory {
 				return moveScore(view, ((DoubleMove) move).secondMove());
 			}
 			else if (move.getClass() == TicketMove.class) {
-				Graph<Integer,Transport> graph = view.getGraph();
-				return (double) graph.getEdgesFrom(graph.getNode(((TicketMove) move).destination())).size();
+				List<Integer> distances = new ArrayList<Integer>();
+				for (Colour colour : view.getPlayers()) {
+					if (colour.isDetective()) {
+						distances.add(dijkstra(view, ((TicketMove) move).destination(), view.getPlayerLocation(colour).get()));
+					}
+				}
+				Collections.sort(distances);
+				return (double) distances.get(0);
 			}
-			return 0.1;
+			return 0.0;
+		}
+
+		public Integer dijkstra(ScotlandYardView view, int location, int targetID) {
+			Graph<Integer,Transport> graph = view.getGraph();
+			Node<Integer> target = graph.getNode(targetID);
+			Set<Node<Integer>> unvisitedNodes = new HashSet<Node<Integer>>(graph.getNodes());
+			Map<Node<Integer>,Integer> distances = new HashMap<Node<Integer>,Integer>();
+			Map<Node<Integer>,Node<Integer>> previousNodes = new HashMap<Node<Integer>,Node<Integer>>();
+			for (Node<Integer> node : unvisitedNodes) {
+				distances.put(node, -1);
+				previousNodes.put(node, null);
+			}
+			distances.put(graph.getNode(location), 0);
+			Node<Integer> currentNode;
+			int minDistance;
+			while (unvisitedNodes.size() > 0) {
+				minDistance = -1;
+				currentNode = null;
+				for (Node<Integer> node : unvisitedNodes) {
+					int  distance = distances.get(node);
+					if (distance >= 0 && (minDistance < 0 || distance < minDistance)) {
+						minDistance = distance;
+						currentNode = node;
+					}
+				}
+				if (minDistance < 0) {
+					break;
+				}
+				unvisitedNodes.remove(currentNode);
+
+				if (currentNode.equals(target)) {
+					break;
+				}
+
+				for (Edge<Integer,Transport> edge : graph.getEdgesFrom(currentNode)) {
+					Node<Integer> destination = edge.destination();
+					if (unvisitedNodes.contains(destination)) {
+						int newDistance = distances.get(currentNode) + 1;
+						if (newDistance < distances.get(destination) || distances.get(destination) < 0) {
+							distances.put(destination, newDistance);
+							previousNodes.put(destination, currentNode);
+						}
+					}
+				}
+			}
+			return distances.get(target);
 		}
 	}
 }
